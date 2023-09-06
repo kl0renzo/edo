13187d66df627e0d2bd8923068fdcdd7c2b4a410
petkosamoletko <ntzotchev@gmail.com>
2023-09-07 01:03:09 +0200

    added matching braces

--- bin/main.ml
+++ bin/main.ml
@@ -4,6 +4,7 @@ open Notty_unix
 
 type cursor_position = int * int
 type modes = Insert | Normal | Command
+type to_color = Normal | Bracet | MatchedWord
 
 type editor = {
   mutable content : string list;
@@ -46,9 +47,9 @@ let mode_to_string mode =
 let update_status editor =
   let x, y = editor.cursor in
   editor.status <-
-    editor.filename  ^ " | "
-    ^ mode_to_string editor.mode ^ " | "
-    ^ string_of_int x ^ "," ^ string_of_int (y + editor.scroll_position)
+    editor.filename ^ " | " ^ mode_to_string editor.mode ^ " | "
+    ^ string_of_int x ^ ","
+    ^ string_of_int (y + editor.scroll_position)
 
 (*
 TODO: limitiations on max cursor positions
@@ -250,15 +251,140 @@ let get_visible_content editor : string list =
   in
   sublist_from_to editor.content start_index end_index
 
+let split_string_with_whitespace input_string =
+  let rec split_acc input acc current_word =
+    match input with
+    | [] -> List.rev (List.append [ current_word ] acc)
+    | first_char :: rest ->
+        if Char.equal ' ' first_char then
+          if current_word = "" then
+            split_acc rest acc (String.make 1 first_char)
+          else if String.ends_with ~suffix:" " current_word then
+            split_acc rest acc (current_word ^ String.make 1 first_char)
+          else split_acc rest (current_word :: acc) " "
+        else if Char.equal '(' first_char || Char.equal ')' first_char then
+          if current_word = "" then
+            split_acc rest (String.make 1 first_char :: acc) ""
+          else
+            split_acc rest (String.make 1 first_char :: current_word :: acc) ""
+        else if String.ends_with ~suffix:" " current_word then
+          split_acc rest (current_word :: acc) (String.make 1 first_char)
+        else split_acc rest acc (current_word ^ String.make 1 first_char)
+  in
+  split_acc (String.to_seq input_string |> List.of_seq) [] ""
+
+let parse_line line = split_string_with_whitespace line
+
+let parse_input editor =
+  let content = get_visible_content editor in
+  let parsed_content = List.map parse_line content in
+  parsed_content
+
+let rec find_position_in_array line cur i pos =
+  match line with
+  | [] -> 0
+  | x :: xs ->
+      if cur + String.length x > pos then i
+      else find_position_in_array xs (cur + String.length x) (i + 1) pos
+
+let highlight_lines lines color_array =
+  let map_line i line ii =
+    (* the colors can be changed depending on the user theme *)
+    match List.nth (List.nth color_array ii) i with
+    | Normal -> I.(string A.(fg white) line)
+    | Bracet -> I.(string A.(fg green ++ bg cyan) line)
+    | MatchedWord -> I.(string A.(fg red) line)
+  in
+
+  List.mapi (fun j ll -> List.mapi (fun i line -> map_line i line j) ll) lines
+
+let find_matching_close_brace content x y =
+  let r = List.rev content in
+  let s = Stack.create () in
+  let iter_lines xx v =
+    if x + xx < List.length content then
+      List.iteri
+        (fun yy vv ->
+          if x + 1 + xx < List.length content || y + 1 + yy < List.length v then
+            match vv with
+            | "(" ->
+                let _ = Stack.pop s in
+                ()
+            | ")" ->
+                Stack.push
+                  (List.length content - xx - 1, List.length v - yy - 1)
+                  s
+            | _ -> ())
+        (List.rev v)
+  in
+  List.iteri iter_lines r;
+  Stack.pop_opt s
+
+let find_matching_open_brace content x y =
+  let s = Stack.create () in
+  let iter_lines xx v =
+    if xx <= x then
+      List.iteri
+        (fun yy vv ->
+          if xx < x || yy < y then
+            match vv with
+            | ")" ->
+                let _ = Stack.pop s in
+                ()
+            | "(" -> Stack.push (xx, yy) s
+            | _ -> ())
+        v
+  in
+  List.iteri iter_lines content;
+  Stack.pop_opt s
+
+let highlight parsed_lines cursor =
+  let codes = List.map (List.map (fun _ -> Normal)) parsed_lines in
+  let line_as_str i = String.concat "" (List.nth parsed_lines i) in
+  match cursor with
+  | y, x when x < List.length parsed_lines && y < String.length (line_as_str x)
+    ->
+      let current_char = String.get (line_as_str x) y in
+      (* find the closing bracket *)
+      if current_char = '(' then
+        let pos = find_position_in_array (List.nth parsed_lines x) 0 0 y in
+        let match_brace = find_matching_close_brace parsed_lines x pos in
+        match match_brace with
+        | Some (a, b) ->
+            List.mapi
+              (fun i x ->
+                if i = a then
+                  (List.mapi (fun ii xx -> if ii = b then Bracet else xx)) x
+                else x)
+              codes
+        | None -> codes
+      else if current_char = ')' then
+        let pos = find_position_in_array (List.nth parsed_lines x) 0 0 y in
+        let match_brace = find_matching_open_brace parsed_lines x pos in
+        match match_brace with
+        | Some (a, b) ->
+            List.mapi
+              (fun i x ->
+                if i = a then
+                  (List.mapi (fun ii xx -> if ii = b then Bracet else xx)) x
+                else x)
+              codes
+        | None -> codes
+      else codes
+  | _, _ -> codes
+
 let rec main_loop editor t =
   let ed = ref editor in
   update_status editor;
-  let status_image = I.string A.(fg black) editor.status in
-  let content = get_visible_content editor in
-  let text_images = List.map (fun line -> I.string A.(fg white) line) content in
+  let status_image = I.string A.(fg blue) editor.status in
+  let parsed_lines = parse_input editor in
+  let highlight_code = highlight parsed_lines editor.cursor in
+  let parsed_highlighted_lines = highlight_lines parsed_lines highlight_code in
+  let text_images = List.map I.hcat parsed_highlighted_lines in
   let combined_text_image = I.vcat text_images in
   let combined_image = I.(status_image <-> hpad 1 0 combined_text_image) in
   let cursor_pos = (fst editor.cursor + 1, snd editor.cursor + 1) in
+
   Term.image t combined_image;
   Term.cursor t (Some cursor_pos);
   match editor.mode with
@@ -328,6 +454,7 @@ let rec main_loop editor t =
       | `End | `Key (`Escape, []) ->
           editor.mode <- Normal;
           main_loop editor t
+      | `Key (`ASCII 'q', _) -> ()
       | _ -> main_loop editor t)
 
 let () =
